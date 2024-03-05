8b992d1bf075fe96466cc4ccaa074fca881f1af4
Nestortion <gerona.nestor@gmail.com>
2024-03-06 00:45:56 +0800

    Add social links and centered footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -9,18 +9,26 @@ import {
 
 function Footer() {
   return (
-    <footer className="h-[5vh] flex justify-between px-4 self-end w-screen items-center ">
-      <div className="flex items-center gap-2 w-fit text-cyan-400">
+    <footer className="h-[5vh] flex justify-between px-4 self-end w-full font-gudea font-bold">
+      <div className="flex-1 flex items-center gap-2 w-fit text-cyan-400">
         <FaCopyright />
         NG Development
       </div>
-      <div className="flex items-center w-fit gap-2 text-cyan-400 text-xl">
-        <FaFacebook />
-        <FaGithub />
-        <FaYoutube />
-        <FaTwitter />
+      <div className="flex-1 flex justify-center items-center w-fit gap-2 text-cyan-400 text-xl">
+        <a href="https://www.facebook.com/Hakuryuukou/">
+          <FaFacebook />
+        </a>
+        <a href="https://github.com/Nestortion">
+          <FaGithub />
+        </a>
+        <a href="">
+          <FaYoutube />
+        </a>
+        <a href="https://twitter.com/ylalet">
+          <FaTwitter />
+        </a>
       </div>
-      <div className="flex items-center w-fit gap-2 text-cyan-400">
+      <div className="flex-1 flex justify-end items-center w-fit gap-2 text-cyan-400">
         Check out my Portfolio! <FaArrowAltCircleRight />
       </div>
     </footer>
