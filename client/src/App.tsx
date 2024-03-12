49aa3b56f86fbff8cbf27d5bced91a0d035f04f4
Nestortion <gerona.nestor@gmail.com>
2024-03-12 16:09:59 +0800

    remove productDisplay page as globallayout child

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import GlobalLayout from "./components/GlobalLayout";
-import ProductsDisplay from "./components/ProductsDisplay";
 
 function App() {
   return (
     <div className=" bg-zinc-200 ">
-      <GlobalLayout>
-        <ProductsDisplay />
-      </GlobalLayout>
+      <GlobalLayout />
     </div>
   );
 }
